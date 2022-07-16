927e5fb9f170c45d68900d0f4e2e1b5491d0e2b6
jrsantur <jrsantur06@gmail.com>
2022-07-16 00:48:10 -0500

    First Commit - Compilacion a prod

---
+++ build/precache-manifest.18c505a42b5e8686e19136bfcb02a61c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac145bc13da6ff3d8f4aba6f13bb3ea1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb4a518cfa4eb3134e83",
+    "url": "/static/css/main.fae71aa2.chunk.css"
+  },
+  {
+    "revision": "c9a8b0fa8a760bf9809d",
+    "url": "/static/js/2.55c71b4c.chunk.js"
+  },
+  {
+    "revision": "fb4a518cfa4eb3134e83",
+    "url": "/static/js/main.f390bc47.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
